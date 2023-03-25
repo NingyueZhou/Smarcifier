3eb7f14f582bc70216c2827c9606ef913ab1ea0a
Nicola Crivellin <nicola.crivellin98@gmail.com>
2023-03-25 14:25:43 +0100

    Add option to set baud rate to read_serial.py
    Use `$ python read_serial.py --rate <RATE> <PORT>` to set a specific
    baud rate. Default is 115200.

--- arduino/read_serial.py
+++ arduino/read_serial.py
@@ -4,13 +4,14 @@ import serial
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("port")
+    parser.add_argument("--rate", default=115200, type=int)
     return parser.parse_args();
 
 def main():
     """Main."""
     args = parse_args()
 
-    with serial.Serial(args.port, timeout=1) as ser:
+    with serial.Serial(args.port, baudrate=int(args.rate), timeout=1) as ser:
         print("Connected to port: ", ser.name)
         while True:
             line = ser.readline().decode().strip("\n\r ")
--- arduino/read_temperature.py
+++ arduino/read_temperature.py
@@ -12,29 +12,34 @@ class History():
         self._buf[self._index] = item
         self._index = (self._index + 1) % len(self._buf)
 
-    def items(self):
-        return self._buf
+    def variance(self):
+        """Calculate the variance over all items in the history."""
+        return np.var(self._buf)
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("port")
-    return parser.parse_args();
+    parser.add_argument("--rate", default=115200, type=int)
+    return parser.parse_args()
 
 def main():
     """Main."""
     args = parse_args()
     history = History(10, 0.0)
 
-    with serial.Serial(args.port, timeout=1) as ser:
+    with serial.Serial(args.port, baudrate=int(args.rate), timeout=1) as ser:
         print("Connected to port: ", ser.name)
         start = time.time()
         while True:
             line = ser.readline().decode().strip("\n\r ")
+            if not line:
+                continue
+
             print(line)
 
             # Calculate variance and decide when to stop measuring
             history.push(float(line))
-            if np.var(history.items()) < 2.0e-5:
+            if history.variance() < 2.0e-5:
                 break
 
         end = time.time()
